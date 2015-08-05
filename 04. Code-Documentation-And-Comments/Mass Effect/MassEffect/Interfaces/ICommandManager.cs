52373477bed087bdc93fea79aa5601b93df3aaac
Katya Marincheva <katya.marincheva@outlook.com>
2015-08-05 14:34:00 +0300

    adding new repository

---
+++ 04. Code-Documentation-And-Comments/Mass Effect/MassEffect/Interfaces/ICommandManager.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ICommandManager.cs" company="X">
+//   X. All rights reserved.
+// </copyright>
+// <summary>
+//   The CommandManager interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MassEffect.Interfaces
+{
+    /// <summary>
+    /// The CommandManager interface.
+    /// </summary>
+    public interface ICommandManager
+    {
+        /// <summary>
+        /// Gets or sets the engine.
+        /// </summary>
+        IGameEngine Engine { get; set; }
+
+        /// <summary>
+        /// The method processing the commands received in the input.
+        /// </summary>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        void ProcessCommand(string command);
+
+        /// <summary>
+        /// Seeds the received commands to the GameEngine.
+        /// </summary>
+        void SeedCommands();
+    }
+}
